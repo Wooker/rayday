08d5d40faf6af51a98db30c2a72aa3495adfaf45
wooker <zakhar.semenov@nu.edu.kz>
2024-06-25 16:57:34 +0500

    Update popup fields

--- src/widgets/popup.rs
+++ src/widgets/popup.rs
@@ -8,11 +8,14 @@ use tui::{
 
 use crate::app::InputMode;
 
-pub const WIDTH: u16 = 30;
-pub const HEIGHT: u16 = 8;
+pub const WIDTH: u16 = 60;
+pub const HEIGHT: u16 = 16;
 
 pub struct PopupAdd<'a> {
-    time: &'a String,
+    start_date: String,
+    start_time: String,
+    end_date: String,
+    end_time: String,
     description: &'a String,
     input_mode: &'a InputMode,
     pub block: Option<Block<'a>>,
@@ -22,7 +25,10 @@ pub struct PopupAdd<'a> {
 impl<'a> PopupAdd<'a> {
     pub fn new(time: &'a String, description: &'a String, input_mode: &'a InputMode) -> Self {
         PopupAdd {
-            time,
+            start_date: String::from(""),
+            start_time: String::from(""),
+            end_date: String::from(""),
+            end_time: String::from(""),
             description,
             input_mode,
             block: None,
@@ -75,17 +81,66 @@ impl<'a> Widget for PopupAdd<'a> {
         };
 
         let layout = Layout::default()
-            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+            .constraints(
+                [
+                    Constraint::Percentage(33),
+                    Constraint::Percentage(33),
+                    Constraint::Percentage(33),
+                ]
+                .as_ref(),
+            )
             .direction(Direction::Vertical)
             .split(block_area);
 
-        let time_par = Paragraph::new(self.time.as_ref())
+        let start_layout = Layout::default()
+            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+            .direction(Direction::Horizontal)
+            .split(layout[0]);
+
+        let end_layout = Layout::default()
+            .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+            .direction(Direction::Horizontal)
+            .split(layout[1]);
+
+        // Start form fields
+        let start_date_par = Paragraph::new(self.start_time.as_ref())
+            .style(match self.input_mode {
+                InputMode::AddingTime => Style::default().fg(Color::Yellow),
+                _ => Style::default(),
+            })
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .title("Start date (YYYY-MM-DD)"),
+            );
+        start_date_par.render(start_layout[0], buf);
+        let start_time_par = Paragraph::new(self.start_time.as_ref())
             .style(match self.input_mode {
                 InputMode::AddingTime => Style::default().fg(Color::Yellow),
                 _ => Style::default(),
             })
             .block(Block::default().borders(Borders::ALL).title("Time (hh:mm)"));
-        time_par.render(layout[0], buf);
+        start_time_par.render(start_layout[1], buf);
+
+        // End form fields
+        let end_date_par = Paragraph::new(self.end_time.as_ref())
+            .style(match self.input_mode {
+                InputMode::AddingTime => Style::default().fg(Color::Yellow),
+                _ => Style::default(),
+            })
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .title("Start date (YYYY-MM-DD)"),
+            );
+        end_date_par.render(end_layout[0], buf);
+        let end_time_par = Paragraph::new(self.end_time.as_ref())
+            .style(match self.input_mode {
+                InputMode::AddingTime => Style::default().fg(Color::Yellow),
+                _ => Style::default(),
+            })
+            .block(Block::default().borders(Borders::ALL).title("Time (hh:mm)"));
+        end_time_par.render(end_layout[1], buf);
 
         let description_par = Paragraph::new(self.description.as_ref())
             .style(match self.input_mode {
@@ -93,6 +148,6 @@ impl<'a> Widget for PopupAdd<'a> {
                 _ => Style::default(),
             })
             .block(Block::default().borders(Borders::ALL).title("Description"));
-        description_par.render(layout[1], buf);
+        description_par.render(layout[2], buf);
     }
 }
