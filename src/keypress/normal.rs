73eff709ccfd468bc59897b3b5994f7ad7d52ed0
wooker <zakhar.semyonov@gmail.com>
2024-06-28 10:46:13 +0500

    add comments to keypress/normal.rs

--- src/keypress/normal.rs
+++ src/keypress/normal.rs
@@ -6,9 +6,9 @@ use crate::{
     widgets::{calendar::CalendarState, event_view::EventViewState},
 };
 
+/// Main handler for the normal input mode
 pub fn handle<'a>(key: KeyEvent, mut app: App<'a>) -> App<'a> {
     match key.code {
-        // KeyCode::Char(c) if key.modifiers == KeyModifiers::CONTROL => app.on_ctrl_key(c),
         KeyCode::Char(c) => on_key(c, app),
         KeyCode::Up => on_up(app),
         KeyCode::Down => on_down(app),
@@ -19,6 +19,7 @@ pub fn handle<'a>(key: KeyEvent, mut app: App<'a>) -> App<'a> {
     }
 }
 
+/// Update calendar state by one week earlier
 pub fn on_up<'a>(mut app: App<'a>) -> App<'a> {
     app.state_calendar = CalendarState::new(
         app.state_calendar
@@ -34,6 +35,7 @@ pub fn on_up<'a>(mut app: App<'a>) -> App<'a> {
     app
 }
 
+/// Update calendar state by one week further
 pub fn on_down<'a>(mut app: App<'a>) -> App<'a> {
     app.state_calendar = CalendarState::new(
         app.state_calendar
@@ -49,6 +51,7 @@ pub fn on_down<'a>(mut app: App<'a>) -> App<'a> {
     app
 }
 
+/// Update calendar state by one day earlier
 pub fn on_left<'a>(mut app: App<'a>) -> App<'a> {
     app.state_calendar = CalendarState::new(
         app.state_calendar
@@ -64,6 +67,7 @@ pub fn on_left<'a>(mut app: App<'a>) -> App<'a> {
     app
 }
 
+/// Update calendar state by one day further
 pub fn on_right<'a>(mut app: App<'a>) -> App<'a> {
     app.state_calendar = CalendarState::new(
         app.state_calendar
@@ -79,6 +83,9 @@ pub fn on_right<'a>(mut app: App<'a>) -> App<'a> {
     app
 }
 
+/// Parse popup input into a new event and saves
+/// it in the db. Then clears popup input and loads
+/// events for selected date.
 pub fn on_add_event<'a>(mut app: App<'a>) -> App<'a> {
     let event = app
         .state_popup
@@ -95,6 +102,7 @@ pub fn on_add_event<'a>(mut app: App<'a>) -> App<'a> {
     app
 }
 
+/// Handle key presses
 pub fn on_key<'a>(c: char, mut app: App<'a>) -> App<'a> {
     match c {
         'q' => {
@@ -105,6 +113,7 @@ pub fn on_key<'a>(c: char, mut app: App<'a>) -> App<'a> {
         'k' => on_up(app),
         'h' => on_left(app),
         'l' => on_right(app),
+        /// Go into select mode
         's' => {
             if app.state_events.events.len() > 0 {
                 app.input_mode = InputMode::Select;
@@ -112,6 +121,7 @@ pub fn on_key<'a>(c: char, mut app: App<'a>) -> App<'a> {
             }
             app
         }
+        /// Go into input mode
         'a' => {
             app.input_mode = InputMode::Input;
             app.state_popup
